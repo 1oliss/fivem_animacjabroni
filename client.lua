7b782fdf8625540191d952e3076473236890c5e6
unknown <czavks@gmail.com>
2022-01-30 02:16:59 +0100

    main

---
+++ client.lua
@@ -0,0 +1,182 @@
+local simples = {
+	`WEAPON_STUNGUN`,
+	`WEAPON_FLAREGUN`,
+
+	`WEAPON_SNSPISTOL`,
+	`WEAPON_SNSPISTOL_MK2`,
+	`WEAPON_VINTAGEPISTOL`,
+	`WEAPON_PISTOL`,
+	`WEAPON_PISTOL_MK2`,
+	`WEAPON_DOUBLEACTION`,
+	`WEAPON_COMBATPISTOL`,
+	`WEAPON_HEAVYPISTOL`,
+
+	`WEAPON_SNOWBALL`,
+	`WEAPON_BALL`,
+	`WEAPON_FLARE`,
+	`WEAPON_FLASHLIGHT`,
+	`WEAPON_KNUCKLE`,
+	`WEAPON_SWITCHBLADE`,
+	`WEAPON_NIGHTSTICK`,
+	`WEAPON_KNIFE`,
+	`WEAPON_DAGGER`,
+	`WEAPON_MACHETE`,
+	`WEAPON_HAMMER`,
+	`WEAPON_WRENCH`,
+	`WEAPON_CROWBAR`,
+
+	`WEAPON_STICKYBOMB`,
+	`WEAPON_MOLOTOV`,
+
+	`WEAPON_DBSHOTGUN`,
+	`WEAPON_SAWNOFFSHOTGUN`,
+	`WEAPON_MICROSMG`,
+	`WEAPON_SMG_MK2`,
+
+	`WEAPON_1911PISTOL`
+}
+
+local types = {
+	[2] = true,
+	[3] = true,
+	[5] = true,
+	[6] = true,
+	[10] = true,
+	[12] = true
+}
+
+local holstered = 0
+local isDead = false
+
+Citizen.CreateThread(function()
+	RequestAnimDict("rcmjosh4")
+	while not HasAnimDictLoaded("rcmjosh4") do
+		Citizen.Wait(75)
+	end
+
+	RequestAnimDict("reaction@intimidation@1h")
+	while not HasAnimDictLoaded("reaction@intimidation@1h") do
+		Citizen.Wait(75)
+	end
+
+	RequestAnimDict("weapons@pistol@")
+	while not HasAnimDictLoaded("weapons@pistol@") do
+		Citizen.Wait(75)
+	end
+
+	while true do
+		Citizen.Wait(75)
+		DisablePlayerVehicleRewards(PlayerId())
+
+		local ped = PlayerPedId()
+		if ped and ped ~= 0 and not isDead and not IsPedInAnyVehicle(ped, false) then
+			local weapon = GetSelectedPedWeapon(ped)
+			if weapon ~= `WEAPON_UNARMED` then
+				if holstered == 0 then
+					local t = 0
+					if `WEAPON_SWITCHBLADE` == weapon then
+						t = 1
+					elseif CheckSimple(weapon) then
+						TaskPlayAnim(ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 5, 0, 0, 0)
+						t = 1
+					elseif types[GetWeaponDamageType(weapon)] then
+						TaskPlayAnim(ped, "reaction@intimidation@1h", "intro", 3.0,1.0, -1, 48, 0, 0, 0, 0)
+						SetCurrentPedWeapon(ped, `WEAPON_UNARMED` , true)
+						t = 2
+					end
+
+					holstered = weapon
+					if t > 0 then
+						if t == 1 then
+							Citizen.Wait(105)
+						elseif t == 2 then
+							Citizen.Wait(105)
+							SetCurrentPedWeapon(ped, weapon, true)
+							Citizen.Wait(105)
+						end
+
+						ClearPedTasks(ped)
+					end
+				elseif holstered ~= weapon then
+					local t, h = 0, false
+					if `WEAPON_SWITCHBLADE` == holstered then
+						Citizen.Wait(15)
+						ClearPedTasks(ped)
+
+						if CheckSimple(weapon) then
+							TaskPlayAnim(ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 5, 0, 0, 0)
+							t = 600
+						elseif types[GetWeaponDamageType(weapon)] then
+							TaskPlayAnim(ped, "reaction@intimidation@1h", "intro", 3.0,1.0, -1, 48, 0, 0, 0, 0)
+							SetCurrentPedWeapon(ped, `WEAPON_UNARMED` , true)
+							h = true
+							t = 1000
+						end
+					elseif `WEAPON_SWITCHBLADE` == weapon then
+						t = 600
+					elseif CheckSimple(holstered) and CheckSimple(weapon) then
+						TaskPlayAnim(ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 5, 0, 0, 0)
+						t = 600
+					elseif types[GetWeaponDamageType(holstered)] and types[GetWeaponDamageType(weapon)] then
+						TaskPlayAnim(ped, "reaction@intimidation@1h", "intro", 3.0,1.0, -1, 48, 0, 0, 0, 0)
+						SetCurrentPedWeapon(ped, holstered, true)
+						h = true
+						t = 1000
+					end
+
+					holstered = weapon
+					if t > 0 then
+						Citizen.Wait(t)
+						if h then
+							SetCurrentPedWeapon(ped, weapon, true)
+							Citizen.Wait(105)
+						end
+
+						ClearPedTasks(ped)
+					end
+				end
+			elseif holstered ~= 0 then
+				local t, h = 0, false
+				if `WEAPON_DOUBLEACTION` == holstered or `WEAPON_SWITCHBLADE` == holstered then
+					t = 1500
+				elseif CheckSimple(holstered) then
+					TaskPlayAnim(ped, "weapons@pistol@", "aim_2_holster", 8.0, 2.0, -1, 48, 5, 0, 0, 0)
+					t = 600
+				elseif types[GetWeaponDamageType(holstered)] then
+					TaskPlayAnim(ped, "reaction@intimidation@1h", "outro", 8.0,2.0, -1, 48, 1, 0, 0, 0)
+					SetCurrentPedWeapon(ped, holstered, true)
+					h = true
+					t = 1500
+				end
+
+				holstered = 0
+				if t > 0 then
+					Citizen.Wait(t)
+					if h then
+						SetCurrentPedWeapon(ped, `WEAPON_UNARMED` , true)
+					end
+
+					ClearPedTasks(ped)
+				end
+			end
+		end
+	end
+end)
+
+function CheckSimple(weapon)
+	for _, simple in ipairs(simples) do
+		if simple == weapon then
+			return true
+		end
+	end
+
+	return false
+end
+
+AddEventHandler('playerSpawned', function()
+	isDead = false
+end)
+
+AddEventHandler('esx:onPlayerDeath', function()
+	isDead = true
+end)
